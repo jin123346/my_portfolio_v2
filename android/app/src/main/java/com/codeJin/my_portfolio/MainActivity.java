4b7851d17240ab8e9212e1845602581efd60aeec
jin123346 <hajhi7899@gmail.com>
2025-03-04 12:19:09 +0900

    Delpoy Flutter web

--- android/app/src/main/java/com/codeJin/my_portfolio/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.codeJin.my_portfolio;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- lib/utils/launchUrl.dart
+++
@@ -1,1 +0,0 @@
-import 'package:url_launcher/url_launcher.dart';
